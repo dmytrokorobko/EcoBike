a371721aab7feff736e54920999a3eb475522784
admin <admin@gmail.com>
2019-12-18 22:54:36 -0500

    stable version

--- src/com/myhome/BinarySearch.java
+++ src/com/myhome/BinarySearch.java
@@ -1,7 +1,17 @@
 package com.myhome;
 
 public class BinarySearch {
-    public void getFoo() {
+    /*
+    Binary method of search. Regarding to description, first is sorting array alphabetically,
+    then search item by dividing array into half each time
+     */
 
+    private String searchLine;
+
+    public BinarySearch() {
+    }
+
+    public void setSearchLine(String searchLine) {
+        this.searchLine = searchLine;
     }
 }
