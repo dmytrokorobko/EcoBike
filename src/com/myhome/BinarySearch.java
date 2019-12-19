1cc7ff65b2e4e30e7c026e5b91f8206c4440611b
admin <admin@gmail.com>
2019-12-18 22:35:08 -0500

    binary search method started

--- src/com/myhome/BinarySearch.java
+++ src/com/myhome/BinarySearch.java
@@ -5,4 +5,13 @@ public class BinarySearch {
     Binary method of search. Regarding to description, first is sorting array alphabetically,
     then search item by dividing array into half each time
      */
+
+    private String searchLine;
+
+    public BinarySearch() {
+    }
+
+    public void setSearchLine(String searchLine) {
+        this.searchLine = searchLine;
+    }
 }
