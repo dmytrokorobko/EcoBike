0fc6b0b328b3440cc94a772fa4a2d95b9cba7bce
admin <admin@gmail.com>
2019-12-18 22:50:13 -0500

    return to stable version

--- src/com/myhome/BinarySearch.java
+++ src/com/myhome/BinarySearch.java
@@ -1,4 +1,17 @@
 package com.myhome;
 
 public class BinarySearch {
+    /*
+    Binary method of search. Regarding to description, first is sorting array alphabetically,
+    then search item by dividing array into half each time
+     */
+
+    private String searchLine;
+
+    public BinarySearch() {
+    }
+
+    public void setSearchLine(String searchLine) {
+        this.searchLine = searchLine;
+    }
 }
--- src/com/myhome/MySearch.java
+++ src/com/myhome/MySearch.java
@@ -6,10 +6,6 @@ import com.myhome.models.FoldingBike;
 import com.myhome.models.Speedelect;
 
 public class MySearch extends Thread {
-    /*
-    My method of search
-     */
-
     private String searchLine;
 
     public MySearch() {
